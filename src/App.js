c9c27f1aac30a50fea2d89e5e150947d4ddf9e9a
Nikhil Sourav <nikhil.rj900@gmail.com>
2021-05-20 15:55:16 +0530

    basic layout setup

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import React from 'react';
 function App() {
   return (
     <div className='App'>
-      <h1>Portfolio</h1>
+      <div className='container-fluid main'>
+        <div className='row glass'>
+          <div className='sidebar'>sidebar</div>
+          <div className='content'>content</div>
+        </div>
+      </div>
+      <div className='circle1'></div>
+      <div className='circle2'></div>
+      <div className='circle3'></div>
+      <div className='circle4'></div>
     </div>
   );
 }
