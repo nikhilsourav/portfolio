5755155ab63935290c2106c01686219e00fb8e3e
Nikhil Sourav <nikhil.rj900@gmail.com>
2021-05-26 09:22:50 +0530

    animated circles

--- src/App.js
+++ src/App.js
@@ -5,10 +5,64 @@ import Sidebar from './components/Sidebar/Sidebar';
 import About from './components/About/About.js';
 import Skills from './components/Skills/Skills.js';
 import Projects from './components/Projects/Projects.js';
-import { AnimatePresence } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 
 function App() {
   const location = useLocation();
+
+  // framer motion
+  const animationVariant_1 = {
+    hidden: {
+      x: 0,
+    },
+    visible: {
+      x: '-5vw',
+      transition: { type: 'just', duration: 20, yoyo: Infinity },
+    },
+    exit: {
+      transition: { type: 'spring' },
+      opacity: 0,
+    },
+  };
+  const animationVariant_2 = {
+    hidden: {
+      x: 0,
+    },
+    visible: {
+      x: '5vw',
+      transition: { type: 'just', duration: 28, yoyo: Infinity },
+    },
+    exit: {
+      transition: { type: 'spring' },
+      opacity: 0,
+    },
+  };
+  const animationVariant_3 = {
+    hidden: {
+      y: 0,
+    },
+    visible: {
+      y: '15vh',
+      transition: { type: 'just', duration: 20, yoyo: Infinity },
+    },
+    exit: {
+      transition: { type: 'spring' },
+      opacity: 0,
+    },
+  };
+  const animationVariant_4 = {
+    hidden: {
+      y: 0,
+    },
+    visible: {
+      y: '-15vh',
+      transition: { type: 'just', duration: 28, yoyo: Infinity },
+    },
+    exit: {
+      transition: { type: 'spring' },
+      opacity: 0,
+    },
+  };
 
   return (
     <div className='App'>
@@ -38,10 +92,32 @@ function App() {
           </div>
         </div>
       </div>
-      <div className='circle1'></div>
-      <div className='circle2'></div>
-      <div className='circle3'></div>
-      <div className='circle4'></div>
+
+      {/* circles */}
+      <motion.div
+        variants={animationVariant_1}
+        initial='hidden'
+        animate='visible'
+        className='circle1'
+      ></motion.div>
+      <motion.div
+        variants={animationVariant_2}
+        initial='hidden'
+        animate='visible'
+        className='circle2'
+      ></motion.div>
+      <motion.div
+        variants={animationVariant_3}
+        initial='hidden'
+        animate='visible'
+        className='circle3'
+      ></motion.div>
+      <motion.div
+        variants={animationVariant_4}
+        initial='hidden'
+        animate='visible'
+        className='circle4'
+      ></motion.div>
     </div>
   );
 }
